f0a9f092798b70b9144a42c28bfe3c883b096972
Marek Kobyliński <Marecki1121@gmail.com>
2019-11-27 21:21:49 +0100

    number of week

---
+++ Lab_2/day number of week/cmake-build-debug/CMakeFiles/hotel_booking.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hotel_booking.dir/main.cpp.o"
+  "hotel_booking.exe"
+  "hotel_booking.pdb"
+  "libhotel_booking.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hotel_booking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lab_2/day number of week/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+    using namespace std;
+int number;
+int main() {
+    cout << "Eloszka, wybierz sobie dzień tygodnia" << endl;
+    switch(number){
+        case 1:
+            cout << "Poniedziałek" << endl;
+            brake;
+        case 2:
+            cout << "Wtorek" << endl;
+            brake;
+        case 3:
+            cout << "Środa" << endl;
+            brake;
+        case 4:
+            cout << "Czwartek" << endl;
+            brake;
+        case 5:
+            cout << "Piątek" << endl;
+            brake;
+        case 6:
+            cout << "Sobota" << endl;
+            brake;
+        case 7:
+            cout << "Niedziela" << endl;
+            brake;
+    }
+    return 0;
+}

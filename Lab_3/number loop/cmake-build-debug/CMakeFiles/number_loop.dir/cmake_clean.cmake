40ddb14baf2817c207d366114dbdb5fd1939ad45
Marek Kobyliński <Marecki1121@gmail.com>
2019-11-28 10:18:26 +0100

    loop 100- & 0+

---
+++ Lab_3/number loop/cmake-build-debug/CMakeFiles/number_loop.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/number_loop.dir/main.cpp.o"
+  "libnumber_loop.dll.a"
+  "number_loop.exe"
+  "number_loop.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/number_loop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lab_3/number loop/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+    using namespace std;
+
+int main() {
+    cout << "Witam, wypiszę Ci parę cyferek, pozwól " << endl;
+//    for( int i = 0; i <= 100; i++)
+//    {
+//        cout << "Twoja licza to: " << i << endl;
+//    }
+
+//    for (int i = 100; i >= 0; i--)
+//        {
+//            cout << "Twoja licza to: " << i << endl;
+//        }
+    return 0;
+}

3ec43402aa82f57206beda5b0805d4e611cfd07e
Marek Kobyliński <Marecki1121@gmail.com>
2019-11-28 13:03:49 +0100

    digits for the range

---
+++ Lab_3/digits from the range/cmake-build-debug/CMakeFiles/digits_from_the_range.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/digits_from_the_range.dir/main.cpp.o"
+  "digits_from_the_range.exe"
+  "digits_from_the_range.pdb"
+  "libdigits_from_the_range.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/digits_from_the_range.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lab_3/digits from the range/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+    using namespace std;
+int numberStart;
+int numberEnd;
+int main() {
+    cout << "Hello, podaj jakiś zakresik i sprawdzimy które lizcby są parzyste" << endl;
+    cout << "Od jakiej cyfry mam zacząć?" << endl;
+    cin >> numberStart;
+    cout << "Na jakiej cyfrze mam skończyć?" << endl;
+    cin >> numberEnd;
+    cout << "Parzyste cyfry w tym przedziale to: ";
+    for (numberStart; numberStart <= numberEnd; numberStart++)
+    {
+        if(numberStart%2==0)
+            cout << numberStart << " ";
+    }
+    return 0;
+}

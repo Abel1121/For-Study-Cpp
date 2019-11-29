624a293f7852f9247774de89030212b5e2a1c1a0
Marek Kobyliński <Marecki1121@gmail.com>
2019-11-29 15:27:47 +0100

    multiplication table

---
+++ Lab_3/multiplication table/cmake-build-debug/CMakeFiles/multiplication_table.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/multiplication_table.dir/main.cpp.o"
+  "libmultiplication_table.dll.a"
+  "multiplication_table.exe"
+  "multiplication_table.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/multiplication_table.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
